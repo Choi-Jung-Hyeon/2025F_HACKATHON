81eb10908542d50a8077d2c335c4df90908e27d7
dodo_kdy <dyn323@naver.com>
2025-09-27 17:33:14 +0900

    deploy

--- backend/my-app/src/main/java/com/example/my_app/project/repository/ProjectRepository.java
+++ backend/my-app/src/main/java/com/example/my_app/project/repository/ProjectRepository.java
@@ -2,7 +2,11 @@ package com.example.my_app.project.repository;
 
 import com.example.my_app.project.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-    
+
 }
